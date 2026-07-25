9e356d3456667f88643550187cad1474f412b0be
Akshitha <akshithareddy1906@gmail.com>
2026-07-25 21:46:00 +0530

    Update frontend API URL for deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://leaddesk-mini-1-df7c.onrender.com/api",
 });
-
 API.interceptors.request.use((config) => {
 
   const token = localStorage.getItem("token");
